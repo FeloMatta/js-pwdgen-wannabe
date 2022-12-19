ad32f7f65905a6969c2e74117a9a60f29f3e6034
FeloMatta <mattafelo@gmail.com>
2022-12-19 10:15:39 +0100

    correzione compito

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
-const name = prompt('Inserisci Qui il tuo nome');
-console.log("il nome dell'utente e':", name);
+//Inizio
 
-const surName = prompt('Inserisci qui il tuo cognome');
-console.log("Il cognome dell'utente e':", surName);
+//1. chiedi all'utente il suo nome,
+const firstName = prompt('Inserisci Qui il tuo nome');
+console.log("il nome dell'utente e':", firstName);
 
+//2. poi chiedi il suo cognome,
+const lastName = prompt('Inserisci qui il tuo cognome');
+console.log("Il cognome dell'utente e':", lastName);
+
+//3. poi chiedi il suo colore preferito
 const favColor = prompt('Inserisci qui il tuo colore preferito');
 console.log("Il colore preferito dell'utente e':", favColor);
 
-document.getElementById('info').innerHTML = name + ' ' + surName + ' ' + favColor + ' ' + '21';
+//4. Infine scrivi sulla pagina nomecognomecolorepreferito21
+let password = firstName + lastName + favColor + '21';
+console.log("la password dell'utente e':", password);
+
+document.getElementById('info').innerHTML = password;
+//Fine
